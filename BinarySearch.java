d55e84d09290dda03ac248d8d6d5323768a7f104
Shafwan Ahmed <116721567+Shafwan23@users.noreply.github.com>
2023-07-08 22:16:04 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,25 @@
+package DSA;
+
+public class BinarySearch {
+	public static void main(String args[]) {
+        int[] num = {-500,-65,-4,0, 1, 4, 7, 44, 66, 78};
+        int target = 4;
+        int ans = binarySearch(num, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] num, int target) {
+        int start = 0;
+        int end = num.length-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target < num[mid]) {
+                end = mid - 1;
+            }else if (target > num[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        } return - 1;
+    }
+}

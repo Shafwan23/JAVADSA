a4b9e590ff460f97f491df9acb38287a63ea5e9d
Shafwan Ahmed <116721567+Shafwan23@users.noreply.github.com>
2023-08-12 22:39:12 +0530

    BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,7 +2,7 @@ package DSA;
 
 public class BinarySearch {
 	public static void main(String args[]) {
-        int[] num = {-500,-65,-4,0, 1, 4, 7, 44, 66, 78};
+        int[] num = {-500,-65,-4,0, 1, 4, 7,23, 44, 66, 78};
         int target = 4;
         int ans = binarySearch(num, target);
         System.out.println(ans);
